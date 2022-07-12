2230c24829ac2418f42cae5895996fe4c4e79d6a
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-07-12 14:09:51 -0500

    get posts and get post by id functional

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,27 @@
 // implement your posts router here
+const express = require('express');
+const router = express.Router();
+
+module.exports = router;
+
+const Posts = require('./posts-model')
+
+
+router.get('/', (req, res) => {
+    Posts.find()
+        .then(posts => res.json(posts))
+        .catch(error => res.status(500).json({message: "The posts information could not be retrieved"}))
+})
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+    Posts.findById(id)
+        .then(post => {
+            if(post == null){
+                res.status(404).json({message: "The post with the specified ID does not exist"})
+                return
+            }
+            res.json(post)
+        })
+        .catch(error => res.status(500).json({message: "The post information could not be retrieved" }))
+})
--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // require your server and launch it here
+const server = require('./api/server')
+
+const port = 9000;
+
+server.listen(port, (req, res) => {
+    console.log(`server running on port ${port}`)
+})
